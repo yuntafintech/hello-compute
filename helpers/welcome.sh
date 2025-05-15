4b7c9eb0908637e9ca728d555215ee4399e05ed3
Sue Smith <sue@benormal.info>
2025-05-15 17:38:53 +0100

    Create welcome.sh

---
+++ helpers/welcome.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script runs when your project starts in the Codespace!
+
+printf "\nHi! Welcome to the Fastly Hello Compute project!\n\nIf you're in a GitHub Codespace, some scripts will run automatically when you start, like building and opening your new blog.\n\nUse the 🔎 Split button at the bottom to see your website and code side by side!\n\nThe README will guide you through next steps. You can close (X) the Terminal while you work... 🐝\n\n"
