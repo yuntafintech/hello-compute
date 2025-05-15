702b4146c88d0525288f252f1ffe8eb59838d219
Sue Smith <sue@benormal.info>
2025-05-15 17:38:24 +0100

    Create publish.sh

---
+++ helpers/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script runs when you hit the Publish button!
+
+printf '🚨 This action will deploy a Compute app to your Fastly account – do you want to proceed? (y/n)? '
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ; then 
+    if [ ! $FASTLY_API_TOKEN ]; then 
+        echo '⚠️ Grab an API key and add it your repo before deploying! Check out the README for steps. 📖' 
+    else 
+        npx --yes @fastly/cli compute publish --accept-defaults --auto-yes || { echo 'Oops! Something went wrong deploying your app.. 🤬'; exit 1; }
+        readarray -t lines < <(npx --yes @fastly/cli domain list --version=latest)
+        IFS='   ' read -r -a array <<< "${lines[1]}"
+        printf "\nWoohoo check out your site at https://${array[2]} 🪩 🛼 🎏\n\n"
+    fi
+else
+    exit 1
+fi
